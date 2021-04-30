a398bdfdedbecf2b185f875a918c90fed1898007
coder-cjl <cjlsire@126.com>
2021-04-30 09:51:29 +0800

    所有权

--- src/main.rs
+++ src/main.rs
@@ -4,26 +4,33 @@ use rand::Rng;
 use std::cmp::Ordering;
 
 fn main() {
-    println!("Guess the number!");
-    let secrect_number = rand::thread_rng().gen_range(1, 101);
-    // println!("The secrect number is {}", secrect_number);
-    loop {
-        println!("Please input your guess");
-        let mut guess = String::new();
-        io::stdin().read_line(&mut guess).expect("Failed to read libe");
-        let guess: u32 = match guess.trim().parse() {
-            Ok(num) => num,
-            Err(_) => continue,
-        };
-        println!("You guessed: {}", guess);
+    // println!("Guess the number!");
+    // let secrect_number = rand::thread_rng().gen_range(1, 101);
+    // // println!("The secrect number is {}", secrect_number);
+    // loop {
+    //     println!("Please input your guess");
+    //     let mut guess = String::new();
+    //     io::stdin().read_line(&mut guess).expect("Failed to read libe");
+    //     let guess: u32 = match guess.trim().parse() {
+    //         Ok(num) => num,
+    //         Err(_) => continue,
+    //     };
+    //     println!("You guessed: {}", guess);
     
-        match guess.cmp(&secrect_number) {
-            Ordering::Less => println!("Too samll"),
-            Ordering::Greater => println!("Too big"),
-            Ordering::Equal => {
-                println!("You win");
-                break;
-            }
-        }
-    }
+    //     match guess.cmp(&secrect_number) {
+    //         Ordering::Less => println!("Too samll"),
+    //         Ordering::Greater => println!("Too big"),
+    //         Ordering::Equal => {
+    //             println!("You win");
+    //             break;
+    //         }
+    //     }
+    // }
+    clone();
+}
+
+fn clone() {
+    let s1 = String::from("hello");
+    let s2 = s1.clone();
+    println!("s1 = {}, s2 = {}", s1, s2);
 }
